43a5517c25818ac3c078ed5acbc30f47c1d4364b
Daan Van Robays <vanrobays.daan@hotmail.com>
2015-03-17 13:44:25 +0100

    Test commit

--- DesktopApplication/DesktopApplication/MainWindow.xaml.cs
+++ DesktopApplication/DesktopApplication/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DesktopApplication
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hallo");
+        }
     }
 }
