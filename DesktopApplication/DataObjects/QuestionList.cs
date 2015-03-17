7135acd91353cf06ae9a1a3a72c5706bc9c7cff5
DrafD <timothy.baert.be@gmail.com>
2015-03-17 15:59:17 +0100

    Stomme fout terug goed maken

--- DesktopApplication/DataObjects/QuestionList.cs
+++ DesktopApplication/DataObjects/QuestionList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace DataObjects
 {
     public class QuestionList
     {
-
+        private IList<Question> questionsList;
 
 
         public QuestionList()
@@ -19,5 +20,25 @@ namespace DataObjects
         public int Id { get; set; }
         public User CurrentUser { get; set; }
         public String Description { get; set; }
+
+        public IList<Question> GetQuestionList()
+        {
+            return questionsList;
+        }
+
+        public void AddQuestion(Question question)
+        {
+            questionsList.Add(question);
+        }
+
+        public void AddQuestions(IList<Question> questions)
+        {
+            foreach (Question quest in questions)
+            {
+                questionsList.Add(quest);
+            }
+        }
+        
+
     }
 }
