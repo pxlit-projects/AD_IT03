ffaaedfcc4e1cec698b8479df36c781f78200374
PieterSwitten <Pieter.Switten@student.pxl.be>
2015-03-17 17:11:55 +0100

    Java login window

--- JavaDesktopApp/src/be/pxl/windows/Start.java
+++ JavaDesktopApp/src/be/pxl/windows/Start.java
@@ -1,10 +1,15 @@
 package be.pxl.windows;
 
+import javax.swing.JFrame;
+
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LoginWindow loginWindow = new LoginWindow();
+		loginWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		loginWindow.setSize(600, 250);
+		loginWindow.setLocationRelativeTo(null);
+		loginWindow.setVisible(true);
 	}
 
 }
