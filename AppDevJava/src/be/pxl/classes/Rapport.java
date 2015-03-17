dd534f8e02c21299105c5f48d858562044191ab5
PieterSwitten <Pieter.Switten@student.pxl.be>
2015-03-17 14:16:56 +0100

    Java project aanpassingen

---
+++ AppDevJava/src/be/pxl/classes/Rapport.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.pxl.classes;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jordy
+ */
+public class Rapport {
+    private int ID;
+       
+    //property QuestionList!!!!
+    //property AnswerList!!!!!
+    
+    private Date date;
+    private int Time;
+    
+    
+    public Rapport() {}
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the Time
+     */
+    public int getTime() {
+        return Time;
+    }
+
+    /**
+     * @param Time the Time to set
+     */
+    public void setTime(int Time) {
+        this.Time = Time;
+    }
+    
+    
+    
+}
