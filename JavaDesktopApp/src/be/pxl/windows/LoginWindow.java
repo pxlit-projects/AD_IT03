ae81c6e6d4aa691a083ebad3c5a4843e2baabb45
PieterSwitten <Pieter.Switten@student.pxl.be>
2015-03-17 15:29:50 +0100

    java windows

---
+++ JavaDesktopApp/src/be/pxl/windows/LoginWindow.java
@@ -0,0 +1,9 @@
+package be.pxl.windows;
+
+import javax.swing.JFrame;
+
+public class LoginWindow extends JFrame {
+	public LoginWindow() {
+		
+	}
+}
---
+++ JavaDesktopApp/src/be/pxl/windows/Start.java
@@ -0,0 +1,10 @@
+package be.pxl.windows;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
