ac70a67e8390609611e9d2a5e03a8ee0962c2151
vncntdm <vdmvinny@hotmail.com>
2015-03-17 17:06:20 +0100

    Altered UserTypeTable_Create
    Changed IF-check to drop table if needed
    Added Auto-inc. for id column

--- SQLQuery/UserTypeTable_Create.sql
+++ SQLQuery/UserTypeTable_Create.sql
@@ -1,15 +1,18 @@
-    IF NOT EXISTS (SELECT * FROM sys.objects 
-        WHERE object_id = OBJECT_ID(N'[dbo].[UserType]') 
-        AND type in (N'U'))
-    BEGIN
-    CREATE TABLE [dbo].[UserType](
-        [id] [int] NOT NULL,
-		[screenname] [nvarchar](50) NOT NULL,
-		[description] [nvarchar](100) NOT NULL,
-     CONSTRAINT [PK_UserType] PRIMARY KEY CLUSTERED   
-    (
-    [id] ASC
-    )WITH (IGNORE_DUP_KEY = OFF)
-    ) 
-    END;
-    GO
+BEGIN
+	IF EXISTS (
+		SELECT * FROM sys.objects 
+		WHERE object_id = OBJECT_ID(N'[dbo].[UserType]') 
+		AND type in (N'U')) 
+		DROP TABLE [dbo].[UserType]
+	
+CREATE TABLE [dbo].[UserType](
+    [id] [int] NOT NULL IDENTITY(1,1),
+	[screenname] [nvarchar](50) NOT NULL,
+	[description] [nvarchar](100) NOT NULL,
+    CONSTRAINT [PK_UserType] PRIMARY KEY CLUSTERED   
+(
+[id] ASC
+)WITH (IGNORE_DUP_KEY = OFF)
+) 
+END;
+GO
