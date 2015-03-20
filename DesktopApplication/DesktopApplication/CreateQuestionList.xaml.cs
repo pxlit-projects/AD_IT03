7b205b69f3f53e2fee5e4002d67c81211ecb7331
Daan Van Robays <vanrobays.daan@hotmail.com>
2015-03-20 14:47:47 +0100

    Update CreateQuestionList

--- DesktopApplication/DesktopApplication/CreateQuestionList.xaml.cs
+++ DesktopApplication/DesktopApplication/CreateQuestionList.xaml.cs
@@ -15,13 +15,43 @@ using System.Windows.Shapes;
 namespace DesktopApplication
 {
     /// <summary>
-    /// Interaction logic for VragenlijstAanmaken.xaml
+    /// Interaction logic for CreateQuestionList.xaml
     /// </summary>
-    public partial class VragenlijstAanmaken : Window
+    public partial class CreateQuestionList : Window
     {
-        public VragenlijstAanmaken()
+        public CreateQuestionList()
         {
             InitializeComponent();
+        }
+        public void GenerateQuestions(string theme, int amount)
+        {
+            Label themeLabel = new Label();
+            themeLabel.Content = theme;
+            themeLabel.FontSize = 24;
+            questionStackPanel.Children.Add(themeLabel);
+            for (int i = 0; i < amount; i++)
+            {
+                Label questionLabel = new Label();
+                TextBox questionTextBox = new TextBox();
+                questionTextBox.Height = 40;
+                questionTextBox.TextWrapping = TextWrapping.Wrap;
+           
+                questionLabel.Content = "Question " + (i + 1);
+                questionStackPanel.Children.Add(questionLabel);
+                questionStackPanel.Children.Add(questionTextBox);
+            }
+        }
+
+        private void NewThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AmountThemeQuestions window = new AmountThemeQuestions();
+            window.Owner = this;
+            window.ShowDialog();
+        }
+
+        private void closeMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
