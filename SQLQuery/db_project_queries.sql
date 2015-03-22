9ebc3f3379473f5a5afe5d0c8f1908af2604406e
Sven V <synbitz@openmailbox.org>
2015-03-22 19:53:58 +0100

    Update db_project_queries.sql
    improved queries

--- SQLQuery/db_project_queries.sql
+++ SQLQuery/db_project_queries.sql
@@ -1,29 +1,22 @@
 /* example queries tested and working.*/
 
 SELECT 
-questionlist.id,
-questionlist.question,
-questionlist.user,
+questionlist.list,
 question.choice,
 question.description,
-question.id,
 question.theme,
 question.title,
 theme.description,
-theme.id,
 theme.title,
-user.id,
 user.type,
 user.firstname,
-user.lastname,
-usertype.id,
-usertype.description
+usertype.screenname
 FROM `questionlist`
 JOIN `question` on questionlist.question = question.id
 JOIN `theme` on question.theme = theme.id
 JOIN `user` on questionlist.user = user.id 
 JOIN `usertype` on user.type = usertype.id
-WHERE theme.title = 'Leren en toepassen van kennis' AND questionlist.id = 1;
+WHERE theme.title = 'Leren en toepassen van kennis' AND questionlist.list = 1;
 /* 
 retrieves the questionlist questions that are part of the first questionlist - and where the theme title is 'Leren en toepassen van kennis'
 it also retrieves the theme , the user who created the questionlist and the type of said user.
@@ -33,7 +26,7 @@ you can easily add and modify this base query to retrieve anything from the data
 
 /* query that retrieves answers */ 
 
-SELECT 
+SELECT
 answerlist.workpoint AS 'Werkpunt',
 answer.title AS 'Antwoord',
 answer.number AS 'Score',
@@ -41,8 +34,9 @@ answer.choice AS 'Multiple choice',
 question.title AS 'Vraag',
 theme.title AS 'Thema',
 user.login AS 'Gebruiker',
-usertype.screenname AS 'Functie'
-
+usertype.screenname AS 'Functie',
+time.datetime AS 'datum',
+time.time AS 'invultijd'
 
 FROM `answerlist`
 LEFT JOIN `answer` on answerlist.answer = answer.id
@@ -50,3 +44,5 @@ LEFT JOIN `question` on answerlist.question = question.id
 LEFT JOIN `theme` on question.theme = theme.id
 LEFT JOIN `user` on answerlist.user = user.id
 LEFT JOIN `usertype` on user.type = usertype.id
+LEFT JOIN `time` on answerlist.user = time.user AND answerlist.list = time.list
+
