eab0a2f07dcd0c85a51f61a30e3592a6735cfb6d
Sven V <synbitz@openmailbox.org>
2015-03-22 20:01:46 +0100

    Update db_project_population.sql
    correction and bugfixes have changed some of the default / test data

--- SQLQuery/db_project_population.sql
+++ SQLQuery/db_project_population.sql
@@ -1,5 +1,3 @@
-
-
 --
 -- Database: `db_project`
 --
@@ -19,21 +17,21 @@ INSERT INTO `answer` (`id`, `title`, `number`, `choice`) VALUES
 -- Dumping data for table `answerlist`
 --
 
-INSERT INTO `answerlist` (`id`, `answer`, `question`, `questionlist`, `workpoint`, `user`) VALUES
-(1, 1, 1, 1, 0, 3),
-(1, 1, 2, 1, 0, 3),
-(1, 1, 3, 1, 0, 3),
-(1, 2, 4, 1, 1, 3),
-(1, 3, 5, 1, 0, 3),
-(1, 4, 6, 1, 0, 3),
-(1, 5, 7, 1, 1, 3),
-(1, 1, 1, 1, 0, 4),
-(1, 1, 2, 1, 0, 4),
-(1, 1, 3, 1, 0, 4),
-(1, 1, 4, 1, 0, 4),
-(1, 1, 5, 1, 0, 4),
-(1, 1, 6, 1, 0, 4),
-(1, 1, 7, 1, 0, 4);
+INSERT INTO `answerlist` (`id`, `list`, `answer`, `question`, `workpoint`, `user`, `client`) VALUES
+(1, 1, 1, 1, 0, 3, 4),
+(2, 1, 1, 2, 0, 3, 4),
+(3, 1, 1, 3, 0, 3, 4),
+(4, 1, 1, 4, 0, 3, 4),
+(5, 1, 1, 5, 0, 3, 4),
+(6, 1, 2, 6, 1, 3, 4),
+(7, 1, 3, 7, 1, 3, 4),
+(8, 1, 1, 1, 0, 4, 4),
+(9, 1, 1, 2, 0, 4, 4),
+(10, 1, 1, 3, 0, 4, 4),
+(11, 1, 3, 4, 1, 4, 4),
+(12, 1, 1, 5, 1, 4, 4),
+(13, 1, 2, 6, 1, 4, 4),
+(14, 1, 1, 7, 0, 4, 4);
 
 --
 -- Dumping data for table `question`
@@ -52,14 +50,14 @@ INSERT INTO `question` (`id`, `title`, `description`, `theme`, `choice`) VALUES
 -- Dumping data for table `questionlist`
 --
 
-INSERT INTO `questionlist` (`id`, `question`, `user`) VALUES
-(1, 1, 1),
-(1, 2, 1),
-(1, 3, 1),
-(1, 4, 1),
-(1, 5, 1),
-(1, 6, 1),
-(1, 7, 1);
+INSERT INTO `questionlist` (`id`, `list`, `question`, `user`) VALUES
+(15, 1, 1, 1),
+(16, 1, 2, 1),
+(17, 1, 3, 1),
+(18, 1, 4, 1),
+(19, 1, 5, 1),
+(20, 1, 6, 1),
+(21, 1, 7, 1);
 
 --
 -- Dumping data for table `theme`
@@ -69,14 +67,22 @@ INSERT INTO `theme` (`id`, `title`, `description`) VALUES
 (1, 'Leren en toepassen van kennis', 'Dit thema gaat over het leren en toepassen van kennis.');
 
 --
+-- Dumping data for table `time`
+--
+
+INSERT INTO `time` (`id`, `datetime`, `list`, `time`, `user`) VALUES
+(1, '2015-03-22 07:26:35', 1, 360, 3),
+(2, '2015-03-22 13:30:22', 1, 720, 4);
+
+--
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `Login`, `firstname`, `lastname`, `screenname`, `password`, `email`, `type`) VALUES
-(1, 'SvenV', 'Sven', 'Vranken', 'Sven', '7777777', 'sven_v45@hotmail.com', 1),
-(2, 'LieveG', 'Lieve', 'Geneesheer', 'Lieve', '7777777', 'lieve.geneesheer@gmail.com', 2),
-(3, 'eenmantelzorger', 'Lara', 'Verzorgster', 'Mev Lara', '7777777', '777@777.com', 3),
-(4, 'eenpatient', 'hersen', 'patiënt', '', '7777777', 'patient@zeker.com', 4);
+INSERT INTO `user` (`id`, `login`, `firstname`, `lastname`, `password`, `email`, `type`) VALUES
+(1, 'SvenV', 'Sven', 'Vranken', '7777777', 'sven_v45@hotmail.com', 1),
+(2, 'LieveG', 'Lieve', 'Geneesheer', '7777777', 'lieve.geneesheer@gmail.com', 2),
+(3, 'eenmantelzorger', 'Lara', 'Verzorgster', '7777777', '777@777.com', 3),
+(4, 'eenpatient', 'hersen', 'patiënt', '7777777', 'patient@zeker.com', 4);
 
 --
 -- Dumping data for table `usertype`
@@ -87,4 +93,3 @@ INSERT INTO `usertype` (`id`, `screenname`, `description`) VALUES
 (2, 'Dokter', 'De geneesheer '),
 (3, 'Mantelzorger', 'Persoon die de patient verzorgt'),
 (4, 'patient', 'persoon die in revalidatie is');
-
