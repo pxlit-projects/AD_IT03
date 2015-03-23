1fd7379fed768073a13ead4cf91ffcb49306b7c2
JordyCollas <jordy.collas@gmail.com>
2015-03-23 22:16:53 +0100

    Add Users

---
+++ DesktopApplication/Database/DBConnect.cs
@@ -0,0 +1,108 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataObjects;
+namespace Database
+{
+
+
+    public class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnect()
+        {
+            server = "81.4.126.109 ";
+            database = "db_project";
+            uid = "luke";
+            password = "lukeluke";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+
+        }
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+             
+                switch (ex.Number)
+                {
+                    case 0:
+                       
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+
+        }
+
+        //Insert statement
+        public void InsertUSers(User User)
+        {
+            String Login = User.Login;
+            String FirstName = User.Firstname;
+            String password = User.Password;
+            String Email = User.Email;
+            //int Type = User.Type;
+
+
+            string query = "INSERT INTO user (login, firstname, password, email, type) VALUES('"+Login + "', '" + FirstName + "', '" +password + "', '" + Email + "', '2')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+
+
+    }
+
+}
--- DesktopApplication/DesktopApplication/UserWindow.xaml.cs
+++ DesktopApplication/DesktopApplication/UserWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DataObjects;
+using Database;
 
 namespace DesktopApplication
 {
@@ -24,5 +26,45 @@ namespace DesktopApplication
             InitializeComponent();
         }
 
+        private void aanmaakButton_Click(object sender, RoutedEventArgs e)
+        {
+            String Naam = naamInput.Text;
+            String Login = inputLogin.Text;
+            String Straat = inputStraat.Text;
+            String Gemeente = inputGemeente.Text;
+            String Postcode = inputPostcode.Text;
+            String Email = inputEmail.Text;
+            String Geboortedatum = inputDatum.ToString();
+           
+
+            int Functie;
+            if (inputHulpverlener.IsChecked == true)
+            {
+                Functie = 2;
+            }
+            else
+            {
+                Functie = 3;
+            }
+
+
+            User newUser = new User();
+
+            newUser.Firstname = Naam;
+            newUser.Login = Login;
+            newUser.Straat = Straat;
+            newUser.Gemeente = Gemeente;
+            newUser.Postcode = Postcode;
+            newUser.Email = Email;
+            newUser.Geboortedatum = Geboortedatum;
+           
+
+
+            DBConnect connectie = new DBConnect();
+            connectie.InsertUSers(newUser);
+
+
+
+        }
     }
 }
