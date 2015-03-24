0bfff7a43887b80b7229dd21a7f487b644d4fc85
DrafD <timothy.baert.be@gmail.com>
2015-03-24 13:26:52 +0100

    UserWindow change

--- DesktopApplication/DesktopApplication/UserWindow.xaml.cs
+++ DesktopApplication/DesktopApplication/UserWindow.xaml.cs
@@ -14,14 +14,51 @@ using System.Windows.Shapes;
 
 namespace DesktopApplication
 {
+
+    enum UserWindowUse
+    {
+        LOOK,
+        EDIT,
+        CREATE
+    };
     /// <summary>
     /// Interaction logic for UserWindow.xaml
     /// </summary>
     public partial class UserWindow : Window
     {
-        public UserWindow()
+        public UserWindow(UserWindowUse use)
         {
             InitializeComponent();
+
+            if (use == UserWindowUse.EDIT)
+                ;
+            else if (use == UserWindowUse.CREATE)
+                ;
+            else
+                ;
+
+        }
+
+        private void create_user(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void reset_form(object sender, RoutedEventArgs e)
+        {
+            this.inputNaam.Clear();
+            this.inputLogin.Clear();
+            this.inputStraat.Clear();
+            this.inputGemeente.Clear();
+            this.inputPostcode.Clear();
+            this.inputEmail.Clear();
+            this.inputDatum.SelectedDate = DateTime.Now;
+            this.userImage.Source = null;
+        }
+
+        private void cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
 
     }
