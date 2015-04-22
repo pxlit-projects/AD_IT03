fedf07772065df680895b9d07be76f5b89112a7c
PieterSwitten <Pieter.Switten@student.pxl.be>
2015-04-22 11:21:38 +0200

    creating Finah-web project + Layout web project

---
+++ finah-web/finah-web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="finah_web.MvcApplication" Language="C#" %>
---
+++ finah-web/finah-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(finah_web.Startup))]
+namespace finah_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
