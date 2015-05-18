c84fcfa3e1583ee41a963dcad92c60f5cb89dc4b
JordyCollas <jordy.collas@gmail.com>
2015-05-18 14:30:58 +0200

    Rapport test

--- Finah-Desktop-Java/src/be/pxl/objects/Theme.java
+++ Finah-Desktop-Java/src/be/pxl/objects/Theme.java
@@ -11,6 +11,8 @@ public class Theme {
 	private String title;
 	private String description;
 
+	public Theme(){};
+	
 	public Theme(int id, String title, String description) {
 		this.id = id;
 		this.title = title;
