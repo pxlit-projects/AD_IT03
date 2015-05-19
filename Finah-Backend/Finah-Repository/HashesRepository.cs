0b3f0ea6f6bdc942c822bd5ee4e16fc21e68f53f
Daan Van Robays <vanrobays.daan@hotmail.com>
2015-05-19 13:47:47 +0200

    Added Hashes Repository and Controller

---
+++ Finah-Backend/Finah-Repository/HashesRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Finah_DomainClasses;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finah_Repository
+{
+    public class HashesRepository
+    {
+        public IEnumerable<hashes> GetHashes()
+        {
+            var context = new db_projectEntities();
+            var hashesList = context.hashes.ToList();
+            return hashesList;
+        }
+
+        public hashes GetHashesById(int id)
+        {
+            var context = new db_projectEntities();
+            var hashesWithId = context.hashes.First(h => h.id == id);
+            return hashesWithId;
+        }
+
+        public hashes AddHashes(hashes newHashes)
+        {
+            using (var context = new db_projectEntities())
+            {
+                if (newHashes == null)
+                {
+                    throw new ArgumentNullException("newHashes");
+                }
+                context.hashes.Add(newHashes);
+                context.SaveChanges();
+                return newHashes;
+            }
+        }
+
+        public Boolean UpdateHashes(int id, hashes hashes)
+        {
+            if (hashes == null || id == null)
+            {
+                return false;
+            }
+            else
+            {
+                using (var context = new db_projectEntities())
+                {
+                    var updatedHashes = context.hashes.FirstOrDefault(h => h.id == id);
+                    updatedHashes.hash = hashes.hash;
+                    updatedHashes.status = hashes.status;
+                    updatedHashes.user = hashes.user;
+                    updatedHashes.date = hashes.date;
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+        }
+
+        public void Deletehashes(int id)
+        {
+            using (var context = new db_projectEntities())
+            {
+                var hashes = context.hashes.Where(h => h.id == id).ToList();
+                foreach (var hash in hashes)
+                {
+                    context.hashes.Remove(hash);
+                }
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Finah-Backend/Finah-WebApi/Controllers/HashesController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Finah_DomainClasses;
+using Finah_Repository;
+using System.Web.Http.Description;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    public class HashesController : ApiController
+    {
+
+        private HashesRepository _hashRepos;
+
+        public HashesController()
+        {
+            _hashRepos = new HashesRepository();
+        }
+
+        // GET: api/Hashes
+        public IEnumerable<hashes> Get()
+        {
+            var hashes = _hashRepos.GetHashes();
+            return hashes;
+        }
+
+        // GET: api/Hashes/5
+        [ResponseType(typeof(hashes))]
+        public async Task<IHttpActionResult> GetHashesById(int id)
+        {
+            var hash = _hashRepos.GetHashesById(id);
+
+            if (hash == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hash);
+        }
+
+        // POST: api/Hashes
+        public HttpResponseMessage Post([FromBody]hashes newHash)
+        {
+
+            if (ModelState.IsValid)
+            {
+                newHash = _hashRepos.AddHashes(newHash);
+                var response = Request.CreateResponse<hashes>(HttpStatusCode.Created, newHash);
+
+                string uri = Url.Link("DefaultApi", new { id = newHash.id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // PUT: api/Hashes/5
+        public HttpResponseMessage Put(int id, [FromBody]hashes updatedHash)
+        {
+            if (!_hashRepos.UpdateHashes(id, updatedHash))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                var response = Request.CreateResponse<hashes>(HttpStatusCode.OK, updatedHash);
+                return response;
+            }
+        }
+
+        // DELETE: api/Hashes/5
+        public HttpResponseMessage Delete(int id)
+        {
+            hashes delHash = _hashRepos.GetHashesById(id);
+            if (delHash == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            _hashRepos.Deletehashes(id);
+            var response = Request.CreateResponse<hashes>(HttpStatusCode.OK, delHash);
+            return response;
+        }
+    }
+}
